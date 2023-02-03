47cbe77562771b066eba0ec520e7c0e316f473d2
vkm0303 <1947055799@qq.com>
2023-02-03 11:31:42 +0800

    chore: remove redundant files

--- scripts/helpers/dist/get_top_posts.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-hexo.extend.helper.register('get_top_posts', function (topK) {
-  var topPosts = hexo.locals.get('posts').filter(function (item) {
-    return item.sticky !== undefined;
-  });
-  return topPosts;
-});
--- scripts/helpers/dist/random_color.dev.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-hexo.extend.helper.register('random_color', function () {
-  var COLOR_CLASSES = ['purple', 'orange', 'red', 'grey', 'blue'];
-  return "rgb(".concat(r, ", ").concat(g, ", ").concat(b, ")");
-});
